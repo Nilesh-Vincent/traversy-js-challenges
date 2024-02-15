a1f94d56acd63fd3e1f9f77a1bc8a0d4dfa92c80
nileshvincent <nileshvincent@gmail.com>
2024-02-15 21:51:08 +0530

    basic 1 completed

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(string) {
+  const lowerCaseString = string.toLowerCase();
+  let count = 0;
+  for (let i = 0; i < lowerCaseString.length; i++) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    if (vowels.includes(lowerCaseString[i])) count = count + 1;
+  }
+
+  return count;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+// function removeDuplicates(array) {
+//   const uniqueArray = [];
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (!uniqueArray.includes(array[i])) {
+//       uniqueArray.push(array[i]);
+//     }
+//   }
+//   return uniqueArray;
+// }
+
+const removeDuplicates = (array) => {
+  return Array.from(new Set(array));
+};
 
 module.exports = removeDuplicates;
