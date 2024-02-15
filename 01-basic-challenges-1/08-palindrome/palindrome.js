1145f09168a21ebf971876049082de6d9ee8f42d
nileshvincent <nileshvincent@gmail.com>
2024-02-15 21:14:22 +0530

    palindrome completed

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -11,12 +11,43 @@
 //   }
 // }
 
-const isPalindrome = (string) => {};
+const isPalindrome = (string) => {
+  const formattedString = removenonAlphaNumeric(string.toLowerCase());
+  const reversedString = reverseString(formattedString);
+  if (reversedString === formattedString) {
+    return true;
+  }
+  return false;
+};
 
-const removeAlphaNumeric = (string) => {};
+const removenonAlphaNumeric = (string) => {
+  let formattedString = "";
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    if (isAlphaNumeric(char)) {
+      formattedString = formattedString + char;
+    }
+  }
 
-const isAlphaNumeric = (char) => {};
+  return formattedString;
+};
 
-const reverseString = (string) => {};
+const isAlphaNumeric = (char) => {
+  const code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) || // Numbers 0-9
+    (code >= 97 && code <= 122) // Lowercase letters a-z
+  );
+};
+
+const reverseString = (string) => {
+  let reveresed = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reveresed = reveresed + string[i];
+  }
+
+  return reveresed;
+};
 
 module.exports = isPalindrome;
