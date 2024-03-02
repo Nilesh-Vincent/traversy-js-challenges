be620eaa97ac839d01ca511b892a7ab69420fbbb
Nilesh-Vincent <nileshvincent@gmail.com>
2024-03-02 10:59:08 +0530

    2-9

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,28 @@
-function diceGameSimulation() {}
+const rollDice = () => {
+  return Math.floor(Math.random() * 6 + 1);
+};
+
+function diceGameSimulation(numberOfSimiulations) {
+  const results = [];
+  for (let i = 0; i < numberOfSimiulations; i++) {
+    const dice1 = rollDice();
+    const dice2 = rollDice();
+    const sum = dice1 + dice2;
+
+    let result = "";
+
+    if (sum === 7 || sum === 11) {
+      result = "win";
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = "lose";
+    } else {
+      result = "roll again";
+    }
+
+    results.push({ dice1, dice2, sum, result });
+  }
+
+  return results;
+}
 
 module.exports = diceGameSimulation;
--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(num) {
+  const formattedNumber = `(${num[0]}${num[1]}${num[2]}) ${num[3]}${num[4]}${num[5]}-${num[6]}${num[7]}${num[8]}${num[9]}`;
+
+  return formattedNumber;
+}
+
+//check other solution specially you can use slice
 
 module.exports = formatPhoneNumber;
